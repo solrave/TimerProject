917be3d6371b2e09fb7db170a5733d71e0f406e3
solrave <demidovsergey777@gmail.com>
2025-12-27 22:00:20 +0100

    2

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,10 +4,13 @@ using Unity.VisualScripting;
 public class Timer
 {
     public event Action<float> PassedTime;
-    public event Action<int> GoneTime;
-    private int _elapsedTime;
+    public event Action<float> GoneTime;
+    private float _elapsedTime;
     private float _passedTime = 0f;
-    public Timer(int elapsedTime)
+    private bool _timerStarted;
+    private bool _timerReset;//
+    private bool _timerStopped;
+    public Timer(float elapsedTime)
     {
         _elapsedTime = elapsedTime;
     }
@@ -16,12 +19,15 @@ public class Timer
     {
         _passedTime += deltaTime;
         PassedTime?.Invoke(_passedTime);
+        StartCountdownTimer(deltaTime);
     }
     public void ResetTimer(){}
     public void StopTimer(){}
     public void ResumeTimer(){}
 
-    private void StartCountdownTimer()
+    private void StartCountdownTimer(float deltaTime)
     {
+        _elapsedTime -= (int)deltaTime;
+        GoneTime?.Invoke(_elapsedTime);
     }
 }
--- Assets/Scripts/TimerView.cs
+++ Assets/Scripts/TimerView.cs
@@ -1,16 +1,20 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class TimerView : MonoBehaviour
 {
+    [SerializeField] private float _elapsedTime;
+    [SerializeField] private float _maxTime;
     [SerializeField] private Button _startButton;
     [SerializeField] private Button _resetButton;
     [SerializeField] private Button _stoptButton;
     [SerializeField] private Button _resumeButton;
     [SerializeField] private Slider _slider;
-    [SerializeField] private RectTransform _countdownView;
+    [SerializeField] private RectTransform _elapsedTimeView;
+    [SerializeField] private RectTransform _passedTimeView;
     [SerializeField] private Image _secondVisualPrefab;
-    [SerializeField] private int _elapsedTime;
     private Timer _timer;
 
     private void Awake()
@@ -20,6 +24,11 @@ public class TimerView : MonoBehaviour
         _resetButton.onClick.AddListener(_timer.ResetTimer);
         _stoptButton.onClick.AddListener(_timer.StopTimer);
         _resumeButton.onClick.AddListener(_timer.ResumeTimer);
+    }
+
+    private void Update()
+    {
+        
     }
 
     private void StartTimer()
